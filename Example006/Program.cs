e4483acd80da72b5dbc3f1aeb502b6b230e2b7ed
Nadezhda <8987813@mail.ru>
2023-01-29 21:07:09 +0300

    Привет Маша-необычно только для Маши

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Привет Маша-необычно только для Маши
+Console.Write ( "Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура! Привет Маша!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine(username);
+}

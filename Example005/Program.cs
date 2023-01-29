b1d3c6102df99aeb03ef4a54e20994fdb664d7c5
Nadezhda <8987813@mail.ru>
2023-01-29 20:44:24 +0300

    Ввод рандомных чмсл и их сумма

---
+++ Example005/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму дву чисел (рандом)
+
+int numberA = new Random().Next(1, 10); //случайное число от 1 до 9
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);

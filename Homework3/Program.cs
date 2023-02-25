6f9a30874d7091b7691665474ec99fdd672a4646
Nadezhda <8987813@mail.ru>
2023-02-25 17:55:35 +0300

    DZ seminar 4

---
+++ Homework3/Program.cs
@@ -0,0 +1,77 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+/*
+Console.Write ("Введите 5 значное число:  ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+int num1 = num % 10;
+int num2 = num % 100 / 10;
+int num3 = num % 1000 / 100;
+int num4 = num % 10000 / 1000;
+int num5 = num % 100000/10000;
+
+int Newnum = (num1 * 10000 + num2 * 1000 + num3 * 100 + num4 * 10 + num5);
+
+if ( num == Newnum)
+{
+  Console.WriteLine ("Yes");
+}
+else
+{
+  Console.WriteLine ("No");
+}
+*/
+
+/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Distance(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    double result = Math.Sqrt(Math.Pow((xB - xA),2)+Math.Pow((yB - yA),2)+Math.Pow((zB - zA),2));
+    return result;
+}
+Console.WriteLine("Input coordinate xA =");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input coordinate yA =");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input coordinate zA =");
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input coordinate xB =");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input coordinate yB =");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input coordinate zB =");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+
+double distanse = Distance(xA, yA, zA, xB, yB, zB);
+Console.WriteLine(distanse); 
+*/
+
+/*_____________________________________________________________
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+
+
+Console.Write ( "Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+    int current = 1;
+
+    while (current <= n)
+    {
+        Console.Write (current *current*current + " ");
+        current += 1;
+    }
+*/
---
+++ Homework4/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+
+Console.Write ("Введите число A:  ");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число B:  ");
+int numberB = Convert.ToInt32 (Console.ReadLine());
+
+double  result = Math.Pow(numberA,numberB);
+Console.WriteLine(result);
+*/
+
+
+/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+
+int SumNumb(int number)
+{
+    int sum = 0;
+    while (number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+Console.Write ("Введите число:  ");
+int num = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Сумма чисел " + SumNumb(num));
+*/
+
+
+/*
+Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+/*int [] arr = { 1, 2, 5, 7, 19 };
+
+void PrintArray ( int [] array)
+{
+    int count = array.Length;
+    for (int i=0; i<count; i++)
+    {
+        Console.Write ( $"{array[i]} " );
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
+*/
+int[] myArray = CreateRandomArray(length, min, max);
+ShowArray(myArray);
+
+
+
